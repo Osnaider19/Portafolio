21f54b5afe4c9216f1fd6b2e940855fc9a0a99ea
osnaider <osnaidermartinez2002@gmail.com>
2023-09-22 16:47:55 -0500

    add proyect spotify-clone

---
+++ src/proyectos/data.js
@@ -0,0 +1,141 @@
+export const proyectos = [
+  {
+    img: "../../../img/cyberpunk.webp",
+    title: "cyberpunk",
+    link: "https://preeminent-liger-266356.netlify.app/",
+    description: `este proyecto se trata de un clon de la pagína de cyberpunk , donde
+    el ususario puede ver los tailer de él juegos y también los
+    episodios de la serie....`,
+    technologies: [
+      {
+        class: "fa-brands fa-html5 icon-skills skill-html",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+    ],
+  },
+  {
+    img: "../../../img/store-games.webp",
+    title: "Store games",
+    link: "https://store-games-site.netlify.app/",
+    description: `Simulando una página de juegos , consumiendo la API de rawg api
+    donde los usuarios pueden buscar videojuegos, ver sus detalles como
+    creadores , calificaciones,tiendas donde comprar.....`,
+    technologies: [
+      {
+        class: "fa-brands fa-react icon-skills skill-react",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+      {
+        class: null,
+        icono: "tailwindcss",
+      },
+    ],
+  },
+  {
+    img: "../../../img/movie.webp",
+    title: "Movie Tv",
+    link: "https://movietvsite.netlify.app/",
+    description: `simulando una página de películas , consumiendo la API de themovietv
+            donde los usuarios pueden buscar películas y ver trailer.....`,
+    technologies: [
+      {
+        class: "fa-brands fa-react icon-skills skill-react",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+      {
+        class: null,
+        icono: "tailwindcss",
+      },
+    ],
+  },
+  {
+    img: "../../../img/landing.webp",
+    title: "Landing Realidad Virtual",
+    link: "https://realityvirtual.netlify.app/",
+    description: `landing page de gafas de realidad virtual donde se muestra el
+            producto y como funciona`,
+    technologies: [
+      {
+        class: null,
+        icono: "Astro",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+      {
+        class: null,
+        icono: "tailwindcss",
+      },
+    ],
+  },
+  {
+    img: "../../../img/fit-nation.webp",
+    title: "gym",
+    link: "https://fitnation.netlify.app/",
+    description: `landing page de una gym por ahora es una landing todavia está en
+            desarrollo la idea es que los usuarios puedan comprar productos de
+            el gym`,
+    technologies: [
+      {
+        class: "fa-brands fa-react icon-skills skill-react",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+      {
+        class: null,
+        icono: "tailwindcss",
+      },
+    ],
+  },
+  {
+    img: "../../../img/spotify.webp",
+    title: "Spotify-Clone",
+    link: "https://spotify-app-react.vercel.app/",
+    description: `spotify con su api donde te puedes connectar a tu cuenta de spotify ver tus listas de reproducción ver playList en tendencias y Albums etc.... aun en desarrollo`,
+    technologies: [
+      {
+        class: "fa-brands fa-react icon-skills skill-react",
+      },
+      {
+        class: "fa-brands fa-css3-alt icon-skills skill-css",
+      },
+      {
+        class: "fa-brands fa-js icon-skills skill-javascript",
+      },
+      {
+        class: null,
+        icono: "tailwindcss",
+      },
+      {
+        class: null,
+        icono: "Redux",
+      },
+      {
+        class: null,
+        icono: "Radix",
+      },
+    ],
+  },
+];
