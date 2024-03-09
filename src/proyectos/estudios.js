5a3de9d0320af84120dc7faa6b3df8971bd40266
osnaider <osnaidermartinez2002@gmail.com>
2024-03-09 14:19:29 -0500

    add estudios

---
+++ src/proyectos/estudios.js
@@ -0,0 +1,17 @@
+export const estudios = [
+  {
+    titulo: "Técnico en desarrollo de software",
+    corporacion: "SENA - Montería (Córdoba) ",
+    fecha: "02/2019 – 12/2019",
+  },
+  {
+    titulo: "Desarrollador Front-end React Js",
+    corporacion: "Alura Latam - Remoto",
+    fecha: "04/2023 – 09/2023",
+  },
+  {
+    titulo: "Curso profesional de React Js",
+    corporacion: "Código facilito - Remoto",
+    fecha: "04/2023 – 04/2023",
+  },
+];
