b4ea9f69a7aa4d56f25fd72d875619a2cbf5f119
osnaider <osnaidermartinez2002@gmail.com>
2024-06-13 13:21:51 -0500

    add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
-export default defineConfig({});
+// If you are not going to use vercel analytics you can delete the analytics
+// If you want to remove vercel from your project you can do npm uninstall vercel
+export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
+
+//If you don't have vercel the configuration will be empty
+
+//ej : export default defineConfig({});
